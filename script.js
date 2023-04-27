85da51e73cb170545a1788f650d0ce7d4bf14f26
Pekeitu <106318905+Pekeitu@users.noreply.github.com>
2023-04-27 09:09:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const respuestas = document.getElementsByTagName('tbody')[0]; //tenemos un solo 
 //const botonAplicarFiltros = document.getElementById('aplicarFiltros');
 const inputName = document.getElementById('filtroNombre');
 const categorias = document.getElementById('categorias');
-
-document.body.onload = () => {
-    cargarElementos();
-    cargarCategorias();
-}
-
+const verMasInfo  = document.getElementById('btn-VerMasInfo');
 // Insertar elementos en el html
 function placeElementos(elementos) {
     respuestas.innerHTML = "";
     elementos.data.products.forEach((element, index) => {
         let nuevaRow = document.createElement('tr');
-        nuevaRow.innerHTML = `<th scope="row">${index+1}</th>\n<td><img class="w-25 h-25" src=${element.thumbnail}></td>\n<td>${element.title}</td>\n<td>${element.rating}</td>\n<td>${element.price}</td>\n<td><button type="button" class="btn btn-info">Ver Mas Info</button></td>`;
+        //nuevaRow.innerHTML = `<th scope="row">${index+1}</th>\n<td><img class="w-25 h-25" src=${element.thumbnail}></td>\n<td>${element.title}</td>\n<td>${element.rating}</td>\n<td>${element.price}</td>\n<td><button type="button" onclick="()" class="btn btn-info">Ver Mas Info</button></td>`;
+        nuevaRow.innerHTML = `
+        <th scope="row">${index+1}</th>
+        \n<td><img class="w-25 h-25" src=${element.thumbnail}></td>
+        \n<td>${element.title}</td>
+        \n<td>${element.rating}</td>
+        \n<td>${element.price}</td>
+        \n<td><button type="button" onclick="cargarModal('${element.id}')" class="btn btn-info">Ver Mas Info</button></td>`;
         respuestas.appendChild(nuevaRow);
     });
 }
@@ -23,6 +25,7 @@ function cargarElementos(){
         .then((response) => {
             placeElementos(response);
         });
+    cargarCategorias();
 }
 
 // Make a request for a user with a given ID
@@ -36,27 +39,53 @@ inputName.onkeyup = (nombre) => {
 }
 
 function cargarCategorias() {
-    let firstElement = document.createElement('a'); //remover filtro
-    firstElement.classList.add('dropdown-item'); firstElement.innerText = "Remover filtro de categorias";
-    firstElement.onclick = cargarElementos;
-    categorias.appendChild(firstElement);
     axios.get('https://dummyjson.com/products/categories')
         .then((response) => {   
             response.data.forEach((element) => {
                 let newCategory = document.createElement('a');
                 newCategory.classList.add('dropdown-item');
                 newCategory.innerText = element;
-                newCategory.onclick = () => {
-                    filtrarCategoria(element);
-                }
                 categorias.appendChild(newCategory);
             });
         })
 }
 
-function filtrarCategoria(val){
-    axios.get('https://dummyjson.com/products/category/' + val)
+function filtrarCategoria(){
+    axios.get('https://dummyjson.com/products/category/' + inputName.value)
         .then((response) => {
             placeElementos(response);
+        })
+}
+
+function cargarModal(id){
+    axios.get('https://dummyjson.com/products/' + id)
+        .then((response) => {
+            let modalTitle = document.getElementById('modal-title');
+            modalTitle.innerText = response.data.title;
+            let modalBody = document.getElementById('modal-body');
+            modalBody.innerHTML = `
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="${response.data.thumbnail}" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="${response.value.images[1]}" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            `
+            $("#modalInfo").show(); 
         })
 }
